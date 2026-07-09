b7f90e411d199fa7677365cd744b3869f8e4b752
Victor Hugo Pinheiro Lima <azullvictorhugo10@gmail.com>
2026-07-09 17:18:38 -0300

    Improving placeOrder

--- src/pages/PlaceOrder.tsx
+++ src/pages/PlaceOrder.tsx
@@ -12,27 +12,81 @@ import { validateOrder, type ZodOrderTypes } from "@/types/zodTypes/orderType"
 
 import { useAddresses } from "@/hooks/UseAddressesHook"
 import { useMutateAddress } from "@/hooks/useMutateAddress"
-import { useCalculateFee,  } from "@/hooks/useCalculateFee"
-
+import { useCalculateFee, } from "@/hooks/useCalculateFee"
+import { useUserIdentifications } from "@/hooks/userIdentifications"
 
 function PlaceOrder() {
-  const {  authenticated } = useContext(AuthContext)!
+  const { authenticated } = useContext(AuthContext)!
   const { cart } = useContext(ShopContext)!
   const mutateAddress = useMutateAddress(authenticated ?? null)
   const { data: addresses, isLoading, error } = useAddresses(authenticated!)
-  const { data: shippingFee, isLoading:loadingFee } = useCalculateFee(authenticated!)
- 
-
- 
+  const { data: shippingFee, isLoading: loadingFee } = useCalculateFee(authenticated!)
+  const { data: userIdentifications } = useUserIdentifications()
 
 
 
 
 
- 
+
+
+
 
 
   const form = validateOrder()
+
+  async function PaymentAssas() {
+
+    if (!addresses) {
+      toast.error('Adicione um endereço de entrega antes de prosseguir com o pagamento.');
+      return;
+    }
+
+
+    if (!userIdentifications?.data.userCpf || !userIdentifications?.data.firstName || !userIdentifications?.data.lastName) {
+      toast.error('Adicione seus dados de identificação antes de prosseguir com o pagamento.');
+      return;
+    }
+
+    if (!shippingFee?.data || shippingFee?.data.length === 0) {
+      toast.error('Não foi possível calcular o frete. Verifique seu endereço e tente novamente.');
+      return;
+    }
+
+    if (!addresses || addresses.userAddresses!.length === 0) {
+      toast.error('Adicione um endereço de entrega antes de prosseguir com o pagamento.');
+      return;
+    }
+
+    try {
+      const response = await api.post("/api/orders/create-asaas", {
+        items: cart,
+        shippingAddress: {
+          street: addresses.userAddresses![0]!.street,
+          city: addresses.userAddresses![0].city,
+          state: addresses.userAddresses![0]!.state,
+          zipCode: addresses.userAddresses![0]!.zipCode,
+          country: addresses.userAddresses![0]!.country,
+          number: addresses.userAddresses![0]!.number,
+          phone: addresses.userAddresses![0]!.phone
+        },
+        paymentMethod: "asaas"
+
+      })
+
+      alert("Pedido criado com sucesso! Você será redirecionado para a página de pagamento.")
+
+      if (response.data.success) {
+        const { checkoutUrl } = response.data;
+        window.location.replace(checkoutUrl);
+      }
+
+    }
+    catch (error) {
+      toast.error('Erro ao criar pedido. Tente novamente.');
+    }
+
+
+  }
 
   async function Payment() {
 
@@ -67,7 +121,6 @@ function PlaceOrder() {
 
     }
     catch (error) {
-      console.error("Error creating order:", error);
       toast.error('Erro ao criar pedido. Tente novamente.');
     }
 
@@ -81,27 +134,31 @@ function PlaceOrder() {
       return;
     }
 
+
+
     try {
-      await mutateAddress.mutateAsync({
+      const responseMutateAddress = await mutateAddress.mutateAsync({
         street: data.street,
         city: data.city,
         state: data.state,
         zipCode: data.zipCode,
         country: data.country,
         phone: data.phone,
-
+        number: data.number
       })
 
-      if (mutateAddress.isSuccess) {
-        toast.success('Endereço adicionado com sucesso!')
+      if (!responseMutateAddress.success) {
+        toast.error(responseMutateAddress.message || 'Erro ao adicionar endereço. Tente novamente.');
+        return;
       }
+      toast.success(responseMutateAddress.message || 'Endereço adicionado com sucesso!')
+
 
 
 
     }
 
     catch (error: any) {
-      console.error("Error adding address:", error.response?.data || error.message);
       toast.error(error.response?.data?.message || 'Erro ao adicionar endereço. Tente novamente.');
     }
 
@@ -111,7 +168,7 @@ function PlaceOrder() {
 
 
   return (
-    <div className="flex bg-r flex-col lg:flex-row lg:justify-center mx-auto">
+    <div className="flex bg-r w-full flex-col lg:flex-row lg:justify-center mx-auto">
       <AddressComponent
         addresses={addresses!}
         isLoading={isLoading}
@@ -121,7 +178,8 @@ function PlaceOrder() {
         onSubmit={onSubmit}
         mutateAddress={mutateAddress}
         fretes={shippingFee?.data}
-        
+        userIdentifications={userIdentifications?.data}
+
       />
 
 
@@ -132,7 +190,7 @@ function PlaceOrder() {
         </div>
 
         <div className="w-full mt-10 flex justify-center">
-          <button onClick={() => Payment()} type="submit" className="px-8 py-3 w-6/12 max-w-[300px] text-center rounded bg-black text-white text-xl">Pagar</button>
+          <button onClick={() => PaymentAssas()} type="submit" className="px-8 py-3 w-6/12 max-w-[300px] text-center rounded bg-black text-white text-xl">Pagar</button>
         </div>
       </div>
     </div>
